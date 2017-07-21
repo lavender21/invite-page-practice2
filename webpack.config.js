03ef59f32913035f986dc985cdf542036f6f6f34
rtning <1573872488@qq.com>
2017-07-21 14:12:54 +0800

    add style to page

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,13 @@ module.exports = {
     entry:'./src/js/index.js',
     output:{
         filename:'bundle.js',
-        path:path.resolve(__dirname,'/dist'),
+        path:path.resolve(__dirname,'./dist'),
         publicPath:'/'
+    },
+    module: {
+        rules:[
+            { test:/\.scss$/,loaders:'style-loader!css-loader!sass-loader'},
+            { test:/\.png$/,loaders:'url-loader'}
+        ]
     }
 };
