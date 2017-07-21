e690dd8aa79eef75b8491f947942da186c3f038f
rtning <1573872488@qq.com>
2017-07-21 10:17:06 +0800

    project init

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+let path = require('path');
+
+module.exports = {
+    entry:'./src/js/index.js',
+    output:{
+        filename:'bundle.js',
+        path:path.resolve(__dirname,'/dist'),
+        publicPath:'/'
+    }
+};
