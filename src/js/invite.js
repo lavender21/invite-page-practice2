36bb9e3a0049d7a33c0e9a9ae397a21fae24f802
rtning <1573872488@qq.com>
2017-07-21 14:27:45 +0800

    add js to checkbox

--- src/js/invite.js
+++ src/js/invite.js
@@ -1,4 +1,7 @@
 let search = document.getElementById('search');
+let main = document.getElementsByTagName('main')[0];
+let checks = document.getElementsByClassName('checks');
+let count = document.getElementById('count');
 
 search.addEventListener('focus',(e)=>{
     search.className = 'search active';
@@ -6,3 +9,17 @@ search.addEventListener('focus',(e)=>{
 search.addEventListener('blur',(e)=>{
     search.className = 'search';
 });
+main.addEventListener('change',(e)=>{
+    if(e.target.className === 'checks'){
+        changeStatus();
+    }
+});
+function changeStatus() {
+    let num = 0;
+    for(let i=0;i<checks.length;i++){
+        if (checks[i].checked){
+            num++;
+        }
+    }
+    count.innerText = num;
+}
