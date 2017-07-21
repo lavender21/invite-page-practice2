c277c86768a3402a332409220219519fcfa265c7
rtning <1573872488@qq.com>
2017-07-21 14:36:39 +0800

    add js to submit

--- src/js/invite.js
+++ src/js/invite.js
@@ -2,6 +2,10 @@ let search = document.getElementById('search');
 let main = document.getElementsByTagName('main')[0];
 let checks = document.getElementsByClassName('checks');
 let count = document.getElementById('count');
+let submit = document.getElementById('submit');
+let invited = '';
+submit.disabled = true;
+submit.className = 'submit disable';
 
 search.addEventListener('focus',(e)=>{
     search.className = 'search active';
@@ -14,12 +18,24 @@ main.addEventListener('change',(e)=>{
         changeStatus();
     }
 });
+submit.addEventListener('click',(e)=>{
+    alert(invited+'has been invited');
+});
 function changeStatus() {
     let num = 0;
+    invited = '';
     for(let i=0;i<checks.length;i++){
         if (checks[i].checked){
             num++;
+            invited += checks[i].parentElement.previousElementSibling.children[0].innerText+' ';
         }
     }
     count.innerText = num;
+    if (num == 0){
+        submit.disabled = true;
+        submit.className = 'submit disable';
+    }else{
+        submit.disabled = false;
+        submit.className = 'submit';
+    }
 }
